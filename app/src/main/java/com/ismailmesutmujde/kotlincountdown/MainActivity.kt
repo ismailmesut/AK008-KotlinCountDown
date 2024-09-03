8f5a7ffd3109b92b08b7f3b829c8d336f7083706
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-03 17:32:50 +0300

    updated: counters

--- app/src/main/java/com/ismailmesutmujde/kotlincountdown/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlincountdown/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.ismailmesutmujde.kotlincountdown
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlincountdown.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingMainActivity: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMainActivity.root
+        setContentView(view)
 
+        // Abstract - Interface - Inheritance
+        object : CountDownTimer (10000,1000) {
+            override fun onTick(p0: Long) {
+                bindingMainActivity.textView.text = "Left : ${p0/1000}"
+            }
+
+            override fun onFinish() {
+                bindingMainActivity.textView.text = "Left : 0"
+            }
+
+        }.start()
     }
 }
